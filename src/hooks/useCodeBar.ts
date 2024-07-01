370def30176698a5ec85567885b249a5c1f502d6
JoshDonis <bjosh.donis@gmail.com>
2024-07-01 17:46:16 -0600

    chore: Add face detection feature using Capacitor ML Kit

--- src/hooks/useCodeBar.ts
+++ src/hooks/useCodeBar.ts
@@ -1,118 +1,123 @@
 import {
-    BarcodeFormat,
-    BarcodeScanner,
-  } from "@capacitor-mlkit/barcode-scanning";
-  import { Capacitor } from "@capacitor/core";
-  import { useEffect } from "react";
-  import { Camera } from "@capacitor/camera";
-  
-  export const useBarCodeScanner = () => {
-    const isGoogleBarcodeScannerModuleAvailable = async () => {
-      try {
-        const { available } =
-          await BarcodeScanner.isGoogleBarcodeScannerModuleAvailable();
-        return available;
-      } catch (error) {
-        throw error;
-      }
-    };
-  
-    const installGoogleBarcodeScannerModule = async () => {
-      try {
-        await BarcodeScanner.installGoogleBarcodeScannerModule();
-      } catch (error) {
-        throw error;
-      }
-    };
-  
-    const checkPermissions = async () => {
-      try {
-        const { camera } = await BarcodeScanner.checkPermissions();
-        return camera;
-      } catch (error) {
-        throw error;
-      }
-    };
-  
-    const requestPermissions = async () => {
-      try {
-        const { camera } = await BarcodeScanner.requestPermissions();
-        return camera;
-      } catch (error) {
-        throw error;
-      }
-    };
-  
-    const requestPermissionsCamera = async () => {
-      try {
-        const { camera } = await Camera.checkPermissions();
-        if(camera !== "granted") {
-          const { camera } = await Camera.requestPermissions({ permissions: ['camera']});
-          return camera;
-        }
-        return camera;
-      } catch (error) {
-        throw error;
-      }
+  BarcodeFormat,
+  BarcodeScanner,
+} from "@capacitor-mlkit/barcode-scanning";
+import { Capacitor } from "@capacitor/core";
+import { useEffect } from "react";
+import { Camera } from "@capacitor/camera";
+export const useBarCodeScanner = () => {
+  const isGoogleBarcodeScannerModuleAvailable = async () => {
+    try {
+      const { available } =
+        await BarcodeScanner.isGoogleBarcodeScannerModuleAvailable();
+      return available;
+    } catch (error) {
+      throw error;
     }
-  
-    const initPlugin = async () => {
-      try {
-        await requestPermissionsCamera();
-        if ((await checkPermissions()) !== "granted") {
-          await requestPermissions();
-          if (!(await isGoogleBarcodeScannerModuleAvailable())) {
-            await installGoogleBarcodeScannerModule();
-          }
-        } else {
-          if (!(await isGoogleBarcodeScannerModuleAvailable())) {
-            await installGoogleBarcodeScannerModule();
-          }
-        }
-      } catch (error: any) {
-        throw error;
-      }
-    };
-  
-    const onScan = async () => {
-      if (!Capacitor.isNativePlatform()) return null;
-      try {
-        const { barcodes } = await BarcodeScanner.scan({
-          formats: [BarcodeFormat.QrCode],
-        });
-        return barcodes;
-      } catch (error: any) {
-        if (error?.message?.includes("scan canceled")) return;
-        console.log({ message: "Error al escanear el código QR", color: "danger" });
-        throw error;
-      }
-    };
-  
-    useEffect(() => {
-      let listener: any;
-      if (Capacitor.isNativePlatform()) {
-        listener = BarcodeScanner.addListener(
-          "barcodeScanned",
-          async (result) => {
-            await listener.remove();
-            document
-              .querySelector("body")
-              ?.classList.remove("barcode-scanner-active");
-            await BarcodeScanner.stopScan();
-          }
-        );
-        initPlugin();
-      }
-  
-      return () => {
-        if (listener) listener.remove();
-      };
-    }, []);
-  
-    return {
-      onScan,
-    };
   };
+
+  const installGoogleBarcodeScannerModule = async () => {
+    try {
+      await BarcodeScanner.installGoogleBarcodeScannerModule();
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const checkPermissions = async () => {
+    try {
+      const { camera } = await BarcodeScanner.checkPermissions();
+      return camera;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const requestPermissions = async () => {
+    try {
+      const { camera } = await BarcodeScanner.requestPermissions();
+      return camera;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const requestPermissionsCamera = async () => {
+    try {
+      const { camera } = await Camera.checkPermissions();
+      if (camera !== "granted") {
+        const { camera } = await Camera.requestPermissions({
+          permissions: ["camera"],
+        });
+        return camera;
+      }
+      return camera;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const initPlugin = async () => {
+    try {
+      await requestPermissionsCamera();
+      if ((await checkPermissions()) !== "granted") {
+        await requestPermissions();
+        if (!(await isGoogleBarcodeScannerModuleAvailable())) {
+          await installGoogleBarcodeScannerModule();
+        }
+      } else {
+        if (!(await isGoogleBarcodeScannerModuleAvailable())) {
+          await installGoogleBarcodeScannerModule();
+        }
+      }
+    } catch (error: any) {
+      throw error;
+    }
+  };
+
+  const onScan = async () => {
+    if (!Capacitor.isNativePlatform()) return null;
+    try {
+      const { barcodes } = await BarcodeScanner.scan({
+        formats: [BarcodeFormat.QrCode],
+      });
+      return barcodes;
+    } catch (error: any) {
+      if (error?.message?.includes("scan canceled")) return;
+      console.log({
+        message: "Error al escanear el código QR",
+        color: "danger",
+      });
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    let listener: any;
+    if (Capacitor.isNativePlatform()) {
+      listener = BarcodeScanner.addListener(
+        "barcodeScanned",
+        async (result) => {
+          await listener.remove();
+          document
+            .querySelector("body")
+            ?.classList.remove("barcode-scanner-active");
+          await BarcodeScanner.stopScan();
+        }
+      );
+      initPlugin();
+    }
+
+    return () => {
+      if (listener) listener.remove();
+    };
+  }, []);
+
   
-  export default useBarCodeScanner;
-  
+
+  return {
+    onScan,
+  };
+};
+
+export default useBarCodeScanner;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,49 @@
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Home.css';
-import useBarCodeScanner from '../hooks/useCodeBar';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Home.css";
+import useBarCodeScanner from "../hooks/useCodeBar";
+import { Camera, CameraResultType } from "@capacitor/camera";
 
 const Home: React.FC = () => {
-  const {onScan} = useBarCodeScanner();
+  const dectectList = [
+    "FACE_DETECTION",
+    "LANDMARK_DETECTION",
+    "LOGO_DETECTION",
+  ];
+  const key = "";
+  const onCamera = async () => {
+    const photo = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.DataUrl,
+    });
+    const body = {
+      requests: [
+        {
+          features: [
+            {
+              type: dectectList[0],
+              maxResults: 10,
+            },
+          ],
+          image: {
+            content: photo?.base64String,
+          },
+        },
+      ],
+    };
+    const resp = await fetch(`https://vision.googleapis.com/v1/images:annotate?key=${key}`, {
+      body: JSON.stringify(body),
+      method: "POST",
+    });
+    console.log(await resp.json());
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -17,8 +57,7 @@ const Home: React.FC = () => {
             <IonTitle size="large">ScanApp</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonButton onClick={onScan}>Scan</IonButton>
-
+        <IonButton onClick={onCamera}>Scan Face</IonButton>
       </IonContent>
     </IonPage>
   );
